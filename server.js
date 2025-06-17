5ee2fc6d92eaa23cf1588365c342bc7901cfc61d
Pumpkintitan <pumpkintitan11@gmail.com>
2025-06-17 17:56:19 -0600

    cors again

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ fastify.register(postgres, {
 });
 
 fastify.register(cors, {
-  origin: ['http://localhost:5173', process.env.FRONTEND_URL],
+  origin: [process.env.FRONTEND_URL],
   methods: ['GET', 'POST'],
 });
 
